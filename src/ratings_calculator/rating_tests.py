9d2c105447cf923ce9ff7b72048f987d3ea61676
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-25 01:03:59 -0500

    add additional test cases

--- src/ratings_calculator/rating_tests.py
+++ src/ratings_calculator/rating_tests.py
@@ -22,6 +22,27 @@ class TestBasicFunctionalities(unittest.TestCase):
         print("New Rating is: ", new_rating)
         self.assertEqual(new_rating, 1200)  # add assertion here
 
+    def test_victor_october_open(self) -> None:
+        # test the calc against perf at october open for 150532
+        ratings = RatingsCalculator()
+        new_rating = ratings.established_ratings(2160, 2014, 3, [2276, 2151, 1972, 2155, 2100])
+        print("New Rating is: ", new_rating)
+        self.assertEqual(round(new_rating), 2073)  # add assertion here
+
+    def test_victor_can_junior_open(self) -> None:
+        # test the calc against perf at october open for 150532
+        ratings = RatingsCalculator()
+        new_rating = ratings.established_ratings(2160, 2068, 2.5, [1949, 1982, 1892, 1843, 1898, 1936])
+        print("New Rating is: ", new_rating)
+        self.assertEqual(round(new_rating), 2014)  # add assertion here
+
+    def test_victor(self) -> None:
+        # test the calc against perf at october open for 150532
+        ratings = RatingsCalculator()
+        new_rating = ratings.established_ratings(2160, 2068, 2.5, [1949, 1982, 1892, 1843, 1898, 1936])
+        print("New Rating is: ", new_rating)
+        self.assertEqual(round(new_rating), 2014)  # add assertion here
+
 
 if __name__ == '__main__':
     unittest.main()
