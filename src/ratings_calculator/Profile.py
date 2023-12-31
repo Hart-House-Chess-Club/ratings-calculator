3e058e1620c44990fea9b28721464f29eb60b881
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-31 09:37:38 -0800

    added config file and lifetime high configuration

--- src/ratings_calculator/Profile.py
+++ src/ratings_calculator/Profile.py
@@ -1,24 +1,26 @@
 """Class to get cfc profile information of the user"""
 import json
 import requests
-
+from config import Config
 
 class CFCProfile:
     """User id of the user that we are trying to get data for"""
 
     # default constructor for ratings calculator
-    def __init__(self, user_id: int, request=True) -> None:
+    def __init__(self, user_id: int, config: Config = Config()) -> None:
         self.user_id = user_id
-        self.profile = self.initialize_profile(request)
+        self.profile = self.initialize_profile()
+        self.use_web = config.web_profile
+        self.quick = config.quick
         return
 
-    def initialize_profile(self, request=True) -> dict:
+    def initialize_profile(self) -> dict:
         """
         Gets the profile of the user
         :param request: boolean indicating whether to use the web to search for this fvalue or not.
         :return: json dictionary mapping of the player and its fields
         """
-        if request:
+        if self.use_web:
             URL = f"https://server.chess.ca/api/player/v1/{self.user_id}"
             page = requests.get(URL)
             return page.json()
@@ -36,16 +38,16 @@ class CFCProfile:
         """
         return self.profile
 
-    def get_games_played(self, regular=True) -> int:
+    def get_games_played(self) -> int:
         """
         Gets the profile of the current user
         :return: json dictionary mapping of the player and its fields
         """
         games = 0
         for tournament in self.profile["player"]["events"]:
-            if tournament["rating_type"] == "R" and regular:
+            if tournament["rating_type"] == "R" and not self.quick:
                 games += tournament["games_played"]
-            elif tournament["rating_type"] == "Q" and not regular:
+            elif tournament["rating_type"] == "Q" and self.quick:
                 games += tournament["games_played"]
             else:
                 # not matching
@@ -58,11 +60,25 @@ class CFCProfile:
         Gets the number of events that this user has participated in
         :return: events that this user has played in
         """
-        numEvents = 0
         if self.profile["player"]["events"] == []:
             return 0
         else:
             return len(self.profile["player"]["events"])
+
+    def get_lifetime_high(self) -> int:
+        """
+        Gets the lifetime high for the event that the user is participating in
+        :return: the lifetime high for this user based on whether quick or regular rating
+        """
+        search_name = "regular_indicator"
+        if self.quick:
+            search_name = "quick_indicator"
+
+        # if the regular indicator is null, that means they haven't played enough games
+        if self.profile[search_name] == []:
+            return 0
+        else:
+            return self.profile[search_name]
 
     def get_last_tournaments(self, num_tournaments: int) -> []:
         """
