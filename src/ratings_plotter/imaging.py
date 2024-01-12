4c854ed07bfe2958a746811412182584e7ce9f7a
Victor Zheng <zhengvictor0@gmail.com>
2024-01-11 22:45:01 -0500

    create version of imaging for ratings with correct axis'

--- src/ratings_plotter/imaging.py
+++ src/ratings_plotter/imaging.py
@@ -12,7 +12,30 @@ class Visualization:
         return
 
     def visualize_ratings(self) -> None:
-        """Visualize ratings for a given user ID"""
+        """Visualize ratings for a given user ID
+        
+        :param id: Represents the id that we want to get
+        """
+        fig, ax = plt.subplots(figsize=(5, 3))
+
+        dateReg = [2000, 2001, 2002, 2003, 2004, 2005, 2006]
+        rtgReg = [200, 500, 500, 600, 1000, 2000, 2400]
+        dateQuick = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2020]
+        rtgQuick = [300, 400, 500, 600, 1030, 2200, 2300, 2350]
+        labels=['Regular Rating', 'Quick Rating',]
+
+        ax.plot(dateReg, rtgReg, ls="solid", label="Regular Rating")
+        ax.plot(dateQuick, rtgQuick, ls="solid", label="Quick Rating")
+        ax.set_title('Ratings over time')
+        ax.legend(loc='upper left')
+        ax.set_ylabel('Rating')
+        ax.set_xlim(xmin=2000, xmax=2020)
+        fig.tight_layout()
+        plt.show()
+
+    def visualize_test(self):
+        """Test ability to visualize using matplotlib
+        """
 
         rng = np.arange(50)
         rnd = np.random.randint(0, 10, size=(3, rng.size))
